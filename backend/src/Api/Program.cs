01b9b8a4cc8c45cd7e430038ccb6e9168c7355ba
randalorian985 <randy@mudbugdigital.com>
2026-04-27 10:51:46 -0500

    Register ApplicationDbContext

--- backend/src/Api/Program.cs
+++ backend/src/Api/Program.cs
@@ -1,8 +1,14 @@
+using JobTicketSystem.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddHealthChecks();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
